53bad08cdfacf6313b30d84d0d934a64f722c8d4
mark <mark@elsa>
2019-07-15 18:19:26 +0200

    Nonnull

--- src/main/java/javagym/Solution.java
+++ src/main/java/javagym/Solution.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.tuple.Pair;
 public class Solution {
 
     @Nonnull
-    public Pair<Point, Point> solve(Point[] points) {
+    public Pair<Point, Point> solve(@Nonnull Point[] points) {
         //TODO: Implement your solution here
         throw new UnsupportedOperationException("solution not yet implemented");
     }
