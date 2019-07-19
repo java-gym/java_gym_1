7dbbe8ba7700614b61baf2b256a35f158d788fe5
mark <mark@elsa>
2019-07-19 13:43:27 +0200

    Resolve a bug where the order of points mattered

--- src/test/java/javagym/SolutionTest.java
+++ src/test/java/javagym/SolutionTest.java
@@ -117,7 +117,7 @@ public class SolutionTest {
         Solution solution = new Solution();
         var otherAnswer = solution.solve(inputPoints);
         assertNotNull(otherAnswer);
-        assertEquals(new Point(1, 0, 0), otherAnswer.getLeft());
-        assertEquals(new Point(2, 0, 0), otherAnswer.getRight());
+        assertTrue(new Point(1, 0, 0).equals(otherAnswer.getLeft()) || new Point(2, 0, 0).equals(otherAnswer.getLeft()));
+        assertTrue(new Point(1, 0, 0).equals(otherAnswer.getRight()) || new Point(2, 0, 0).equals(otherAnswer.getRight()));
     }
 }
